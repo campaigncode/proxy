b8b8f64125e3d6a41e7c752b8fba6e9384e7b0a6
RahulR100 <rahulrao@email.com>
2023-01-01 15:35:28 -0800

    move routes

--- index.js
+++ index.js
@@ -8,11 +8,26 @@ const morgan = require("morgan");
 // Creating express server
 const app = express();
 
+// Logging the requests
+app.use(morgan("combined"));
+cache.on("message", (message) => {
+  console.log(message);
+});
+
 // Create CORS Anywhere server
 const CORS_PROXY_PORT = 5000;
 corsAnywhere.createServer({}).listen(CORS_PROXY_PORT, () => console.log(`Internal CORS Anywhere server started at port ${CORS_PROXY_PORT}`));
 
-// Proxy to CORS server
+// Use cache first
+// s, m, l define cache times
+app.get("/s/*", cache.route(10));
+app.get("/m/*", cache.route(60));
+app.get("/l/*", cache.route(600));
+app.options("/s/*", cache.route(10));
+app.options("/m/*", cache.route(60));
+app.options("/l/*", cache.route(600));
+
+// Else proxy to CORS server
 app.use(
   proxy(`localhost:${CORS_PROXY_PORT}`, {
     proxyReqPathResolver: (req) => {
@@ -25,17 +40,3 @@ const APP_PORT = process.env.PORT;
 app.listen(APP_PORT, () => {
   console.log(`External CORS cache server started at port ${APP_PORT}`);
 });
-
-// Logging the requests
-app.use(morgan("combined"));
-cache.on("message", (message) => {
-  console.log(message);
-});
-
-// s, m, l define cache times
-app.get("/s/*", cache.route(10));
-app.get("/m/*", cache.route(60));
-app.get("/l/*", cache.route(600));
-app.options("/s/*", cache.route(10));
-app.options("/m/*", cache.route(60));
-app.options("/l/*", cache.route(600));
